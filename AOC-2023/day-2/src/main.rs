d1f844b294d90dad6c0e586028c00af43474b327
griff613 <griffith.zach13@gmail.com>
2024-04-07 09:34:10 -0600

    start day2

--- AOC-2023/day-2/src/main.rs
+++ AOC-2023/day-2/src/main.rs
@@ -16,11 +16,41 @@ fn read_file(file_name: &str) -> io::Result<Vec<String>> {
     reader.lines().collect()
 }
 
-fn process_input(file_name: &str) -> HashMap<i32, bool> {
-    
+fn solve_day1(parts: Vec<&str>) -> bool {
     let max_red = 12;
     let max_green = 13;
     let max_blue = 14;
+
+    let mut game_possible = true;
+
+    for part in parts {
+        let reveals: Vec<&str> = part.split(',').collect(); 
+        for reveal in reveals {
+            let values: Vec<&str> = reveal.split_whitespace().collect();
+            let num_i32: Result<i32, _> = values[0].parse();
+            let color = values[1];
+            match num_i32 {
+                Ok(n) => {
+                    if(color == "red" && n > max_red) ||
+                    (color == "green" && n > max_green) ||
+                    (color == "blue" && n > max_blue) {
+                        game_possible = false;
+                    }
+                }
+                Err(_) => println!("Error checking colors"),
+            }                
+            println!("{:?}", reveal.trim());
+        }
+    }
+    game_possible
+}
+
+fn solve_day2(parts: Vec<&str>) -> {
+    
+}
+
+fn process_input(file_name: &str) -> HashMap<i32, bool> {
+    
 
     // Keep a map of the game number and whether or not it was possible
     let mut game_map: HashMap<i32, bool> = HashMap::new();
@@ -28,8 +58,6 @@ fn process_input(file_name: &str) -> HashMap<i32, bool> {
     let lines = read_file("input.txt").expect("parse error");
     for line in lines {
     
-        let mut game_possible = true;
-
         // get rid of 'game #:'
         let stripped_line = match line.find(':') {
             Some(index) => &line[index + 1..].trim(),
@@ -37,25 +65,7 @@ fn process_input(file_name: &str) -> HashMap<i32, bool> {
         };
 
         let parts: Vec<&str> = stripped_line.split(';').collect();
-        for part in parts {
-            let reveals: Vec<&str> = part.split(',').collect(); 
-            for reveal in reveals {
-                let values: Vec<&str> = reveal.split_whitespace().collect();
-                let num_i32: Result<i32, _> = values[0].parse();
-                let color = values[1];
-                match num_i32 {
-                    Ok(n) => {
-                        if(color == "red" && n > max_red) ||
-                        (color == "green" && n > max_green) ||
-                        (color == "blue" && n > max_blue) {
-                            game_possible = false;
-                        }
-                    }
-                    Err(_) => println!("Error checking colors"),
-                }                
-                println!("{:?}", reveal.trim());
-            }
-        }
+        let game_possible = solve_day1(parts);
         game_map.insert(game_number, game_possible);
         game_number += 1;
     }
